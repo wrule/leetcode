4c119c52434accd983baa7d63134541b2239984a
jimao <jimao@jimaodeMBP.lan>
2022-02-06 14:38:34 +0800

    穷举求解

---
+++ src/twosum/index.ts
@@ -0,0 +1,15 @@
+
+function
+twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length - 1; ++i) {
+    const a = nums[i];
+    for (let j = i + 1; j < nums.length; ++j) {
+      const b = nums[j];
+      if (a + b === target) {
+        return [i, j];
+      }
+    }
+  }
+  return null;
+}
+
