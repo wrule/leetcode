cfa3635f3066278a3dac901bc06b255963f5435d
jimao <shihao.gu@perfma.com>
2024-10-08 13:24:44 +0800

    canCompleteCircuit

--- 134.ts
+++ 134.ts
@@ -1,2 +1,7 @@
 
-console.log(1234);
+function canCompleteCircuit(gas: number[], cost: number[]): number {
+  return -1;
+}
+
+console.log(canCompleteCircuit([1,2,3,4,5], [3,4,5,1,2]));
+console.log(canCompleteCircuit([2,3,4], [3,4,3]));
