525879352f7d1e46e51263f32d84f37549ae5b3c
jimao <shihao.gu@perfma.com>
2024-10-09 12:06:10 +0800

    类构造

--- 144.ts
+++ 144.ts
@@ -12,6 +12,17 @@
  * }
  */
 
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val == null ? 0 : val);
+    this.left = (left == null ? null : left);
+    this.right = (right == null ? null : right);
+  }
+}
+
 function preorderTraversal(root: TreeNode | null): number[] {
-    
+  return [];
 };
