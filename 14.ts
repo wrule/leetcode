1df58942f2da058647fd8363ff000481f834b92a
jimao <shihao.gu@perfma.com>
2024-10-10 11:43:17 +0800

    14

--- 14.ts
+++ 14.ts
@@ -3,14 +3,17 @@
 // 如果不存在公共前缀，返回空字符串 ""。
 
 function longestCommonPrefix(strs: string[]): string {
-  if (strs.length < 1) return '';
-  const first = strs[0];
+  const first = strs[0] ?? '';
   let result = '';
   for (let i = 1; i <= first.length; ++i) {
     const subStr = first.slice(0, i);
-    console.log(subStr);
+    if (strs.every((str) => str.startsWith(subStr))) {
+      result = subStr;
+    } else {
+      break;
+    }
   }
   return result;
 }
 
-longestCommonPrefix(['束带结发', '']);
+console.log(longestCommonPrefix(['束带结发', '束带1结']));
