2c68136ba5aa9be0b9225d77cb70359577c826ac
jimao <shihao.gu@perfma.com>
2024-10-10 12:35:39 +0800

    20

--- 20.ts
+++ 20.ts
@@ -6,6 +6,32 @@
 // 左括号必须以正确的顺序闭合。
 // 每个右括号都有一个对应的相同类型的左括号。
 
+
 function isValid(s: string): boolean {
-  return true;
+  const charArray = Array.from(s);
+  const charStack: string[] = [];
+  for (let i = 0; i < charArray.length; ++i) {
+    const char = charArray[i];
+    if (char === '(') {
+      charStack.push(char);
+    }
+    if (char === ')') {
+      if (charStack.pop() !== '(') return false;
+    }
+    if (char === '{') {
+      charStack.push(char);
+    }
+    if (char === '}') {
+      if (charStack.pop() !== '{') return false;
+    }
+    if (char === '[') {
+      charStack.push(char);
+    }
+    if (char === ']') {
+      if (charStack.pop() !== '[') return false;
+    }
+  }
+  return charStack.length === 0;
 }
+
+console.log(isValid('()[]{}'));
