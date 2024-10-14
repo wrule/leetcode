b3edd44061a39f687a967f6d61b7dc804f6ebfbe
jimao <shihao.gu@perfma.com>
2024-10-14 10:30:38 +0800

    2

--- 2.ts
+++ 2.ts
@@ -16,6 +16,32 @@
  * }
  */
 
-function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+  }
+}
 
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  let result = new ListNode();
+  let current1 = l1;
+  let current2 = l2;
+  let currentResult = result;
+
+  let carry = 0;
+  while (current1 || current2) {
+    const sum = (current1?.val ?? 0) + (current2?.val ?? 0) + carry;
+    const num = sum % 10;
+    currentResult.next = new ListNode(num);
+    carry = Math.floor(sum / 10);
+    current1 = current1?.next ?? null;
+    current2 = current2?.next ?? null;
+    currentResult = currentResult.next;
+  }
+  if (carry > 0) currentResult.next = new ListNode(carry);
+
+  return result.next;
 }
