6ace2512684fbc05bb3968bd97a6e593beb0e8cf
jimao <shihao.gu@perfma.com>
2024-10-15 12:54:11 +0800

    83

---
+++ 83.ts
@@ -0,0 +1,35 @@
+// 给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+  }
+}
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  let result = head;
+  let current = head;
+  while (current) {
+    if (current.val === current.next?.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return result;
+}
