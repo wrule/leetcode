23e18cce35a7951b9629c9c1cd0a2f2fde937261
jimao <shihao.gu@perfma.com>
2024-10-15 20:31:35 +0800

    没有去重

--- 15.ts
+++ 15.ts
@@ -3,26 +3,27 @@
 // 注意：答案中不可以包含重复的三元组。
 
 function threeSum(nums: number[]): number[][] {
+  const result: number[][] = [];
   nums.sort();
-  let leftIndex = 0;
-  let rightIndex = nums.length - 1;
-  let result: number[][] = [];
-  while (leftIndex < rightIndex - 1) {
-    const leftNum = nums[leftIndex];
-    const rightNum = nums[rightIndex];
-    for (let middleIndex = leftIndex + 1; middleIndex < rightIndex; ++middleIndex) {
-      const middleNum = nums[middleIndex];
-      const sum = leftNum + middleNum + rightNum;
-      if (sum < 0) {
-
-      } else if (sum > 0) {
-
+  for (let startIndex = 0; startIndex < nums.length - 2; ++startIndex) {
+    let leftIndex = startIndex + 1;
+    let rightIndex = nums.length - 1;
+    while (leftIndex < rightIndex) {
+      const startNum = nums[startIndex];
+      const leftNum = nums[leftIndex];
+      const rightNum = nums[rightIndex];
+      const sum = startNum + leftNum + rightNum;
+      if (sum > 0) {
+        rightIndex--;
+      } else if (sum < 0) {
+        leftIndex++;
       } else {
-        result.push([leftIndex, middleIndex, rightIndex]);
+        result.push([startNum, leftNum, rightNum]);
+        leftIndex++;
       }
     }
   }
-  return [];
+  return result;
 }
 
 console.log(threeSum([-1,0,1,2,-1,-4]))
