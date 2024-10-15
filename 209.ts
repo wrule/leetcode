b6ad835b265c960dba05e0151b9e2493aa24239b
jimao <shihao.gu@perfma.com>
2024-10-15 20:51:25 +0800

    209

---
+++ 209.ts
@@ -0,0 +1,11 @@
+// 给定一个含有 n 个正整数的数组和一个正整数 target 。
+
+// 找出该数组中满足其总和大于等于 target 的长度最小的 
+// 子数组
+//  [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+
+function minSubArrayLen(target: number, nums: number[]): number {
+  return 0;    
+}
+
+console.log(minSubArrayLen(7, [2,3,1,2,4,3]));
