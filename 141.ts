fae7debafaa9a3d8ea7c99aedbc092890413e39b
jimao <shihao.gu@perfma.com>
2024-10-16 10:24:10 +0800

    141

---
+++ 141.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+  }
+}
+
+function hasCycle(head: ListNode | null): boolean {
+  let result = false;
+  let current = head;
+  while (current) {
+    
+  }
+  return result;
+}
