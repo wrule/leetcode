7e561c9fa07ae1d3f2d3e32983f2a722702b8f9e
jimao <shihao.gu@perfma.com>
2024-10-16 12:35:24 +0800

    141

--- 141.ts
+++ 141.ts
@@ -10,14 +10,14 @@
  * }
  */
 
-class ListNode {
-  val: number
-  next: ListNode | null
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
-  }
-}
+// class ListNode {
+//   val: number
+//   next: ListNode | null
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.next = (next===undefined ? null : next)
+//   }
+// }
 
 function hasCycle(head: ListNode | null): boolean {
   let fastCurrent = head;
