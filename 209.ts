7054a05266036872f0f17c1a034ab5876e7664d3
jimao <shihao.gu@perfma.com>
2024-10-16 18:03:26 +0800

    209

--- 209.ts
+++ 209.ts
@@ -5,26 +5,23 @@
 //  [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
 
 function minSubArrayLen(target: number, nums: number[]): number {
-
+  let leftIndex = 0;
+  let rightIndex = 0;
   let lengthMin = Infinity;
-
-  for (let leftIndex = 0; leftIndex < nums.length;) {
-    for (let rightIndex = 0; rightIndex < nums.length;) {
-      const sum = nums[leftIndex] + nums[rightIndex];
-      if (sum < target) {
-
-      } else {
-        const length = rightIndex - leftIndex + 1;
-        if (length === 1) return 1;
-        if (length < lengthMin) {
-          lengthMin = length;
-        }
-        break;
-      }
+  let sum = nums[0];
+  while (rightIndex < nums.length) {
+    if (sum < target) {
+      rightIndex++;
+      sum += nums[rightIndex];
+    } else {
+      const length = rightIndex - leftIndex + 1;
+      if (length === 1) return 1;
+      if (length < lengthMin) lengthMin = length;
+      sum -= nums[leftIndex];
+      leftIndex++;
     }
   }
-
-  return 0;
+  return lengthMin === Infinity ? 0 : lengthMin;
 }
 
 console.log(minSubArrayLen(7, [2,3,1,2,4,3]));
