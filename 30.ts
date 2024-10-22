2287466e53a2adbfcf6d27378de7cbfc07af8bc6
jimao <shihao.gu@perfma.com>
2024-10-22 11:04:49 +0800

    提交

--- 30.ts
+++ 30.ts
@@ -5,6 +5,31 @@
 // 例如，如果 words = ["ab","cd","ef"]， 那么 "abcdef"， "abefcd"，"cdabef"， "cdefab"，"efabcd"， 和 "efcdab" 都是串联子串。 "acdbef" 不是串联子串，因为他不是任何 words 排列的连接。
 // 返回所有串联子串在 s 中的开始索引。你可以以 任意顺序 返回答案。
 
+class Queue {
+  public constructor(private readonly limit: number) { }
+
+  private queue: string[] = [];
+
+  public get size() {
+    return this.queue.length;
+  }
+
+  public get joinStr() {
+    return this.queue.join('');
+  }
+
+  public add(str: string) {
+    this.queue.push(str);
+    if (this.queue.length > this.limit) {
+      this.shift();
+    }
+  }
+
+  public shift() {
+    return this.queue.shift();
+  }
+}
+
 class QueueSet {
   public constructor(private readonly limit: number) { }
 
@@ -51,14 +76,13 @@ function findSubstring(s: string, words: string[]): number[] {
   const result: number[] = [];
   const wordsSet = new Set(words);
   const wordLength = words[0].length;
-  const charWindow = new QueueSet(wordLength);
+  const charWindow = new Queue(wordLength);
   const charArray = Array.from(s);
   for (let rightIndex = 0; rightIndex < charArray.length; ++rightIndex) {
     charWindow.add(charArray[rightIndex]);
     if (wordsSet.has(charWindow.joinStr)) {
-      // console.log(charWindow.joinStr);
+      console.log(charWindow.joinStr);
     }
-    console.log(charWindow.joinStr);
   }
   return result;
 }
