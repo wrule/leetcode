cccc98a3f7e6bb4b82c2823cdaf5abbf868ff682
jimao <shihao.gu@perfma.com>
2024-10-22 19:10:51 +0800

    提交代码

--- 36.ts
+++ 36.ts
@@ -8,5 +8,21 @@
 // 空白格用 '.' 表示。
  
 function isValidSudoku(board: string[][]): boolean {
-  return false;
+  const rows: any[] = Array(9).fill({ });
+  const cols: any[] = Array(9).fill({ });
+  const girds: any[] = Array(9).fill({ });
+  for (let y = 0; y < 9; ++y) {
+    for (let x = 0; x < 9; ++x) {
+      const num = board[y][x];
+      if (num === '.') continue;
+      const rowValue = rows[y][num] = (rows[y][num] ?? 0) + 1;
+      if (rowValue > 1) return false;
+      const colValue = cols[x][num] = (rows[x][num] ?? 0) + 1;
+      if (colValue > 1) return false;
+      const girdIndex = Math.floor(y / 3) * 3 + Math.floor(x / 3);
+      const girdValue = girds[girdIndex][num] = (girds[girdIndex][num] ?? 0) + 1;
+      if (girdValue > 1) return false;
+    }
+  }
+  return true;
 }
