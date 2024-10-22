499dbbf70a52d608caab343efa12961f496457b7
jimao <shihao.gu@perfma.com>
2024-10-22 10:56:08 +0800

    30

--- 30.ts
+++ 30.ts
@@ -49,7 +49,13 @@ class QueueSet {
 
 function findSubstring(s: string, words: string[]): number[] {
   const result: number[] = [];
+  const wordLength = words[0].length;
+  const charWindow = new QueueSet(wordLength);
   const charArray = Array.from(s);
+  for (let rightIndex = 0; rightIndex < charArray.length; ++rightIndex) {
+    charWindow.add(charArray[rightIndex]);
+    console.log(charWindow.joinStr);
+  }
   return result;
 }
 
