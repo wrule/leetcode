5b42be5632043c91cc38511dbac65c9f44060637
jimao <shihao.gu@perfma.com>
2024-10-22 11:49:50 +0800

    76

--- 76.ts
+++ 76.ts
@@ -4,30 +4,42 @@
 // 如果 s 中存在这样的子串，我们保证它是唯一的答案。
 
 class CountMap {
-  public constructor() { }
+  public constructor(private readonly limit: number) { }
 
   private count = 0;
-  private strMap = new Map<string, number>();
+  private queue: string[] = [];
+  private countMap = new Map<string, number>();
 
   public inc(str: string) {
-    this.strMap.set(str, (this.strMap.get(str) ?? 0) + 1);
+    this.countMap.set(str, (this.countMap.get(str) ?? 0) + 1);
     this.count++;
   }
 
   public dec(str: string) {
-    this.strMap.set(str, (this.strMap.get(str) ?? 0) - 1);
+    this.countMap.set(str, (this.countMap.get(str) ?? 0) - 1);
     this.count--;
   }
 
   public has(str: string) {
-    return this.strMap.has(str);
+    return this.countMap.has(str);
+  }
+
+  public push(str: string) {
+    this.queue.push(str);
+    this.dec(str);
+    if (this.queue.length > this.limit) this.shift();
+  }
+
+  public shift() {
+    const rmStr = this.queue.shift();
+    if (rmStr) this.inc(rmStr);
   }
 }
 
 function minWindow(s: string, t: string): string {
   const sCharArray = Array.from(s);
   const tCharArray = Array.from(t);
-  const tCharMap = new CountMap();
+  const tCharMap = new CountMap(tCharArray.length);
   tCharArray.forEach((char) => {
     tCharMap.inc(char);
   });
@@ -37,6 +49,8 @@ function minWindow(s: string, t: string): string {
     const char = sCharArray[rightIndex];
     if (tCharMap.has(char)) {
       console.log(char, rightIndex);
+      tCharMap.push(char);
+      console.log(tCharMap);
     }
   }
   return '';
