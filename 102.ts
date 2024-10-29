8417636b24a4ebd83c86d8c663d7b691b010741d
jimao <shihao.gu@perfma.com>
2024-10-29 12:24:56 +0800

    102

---
+++ 102.ts
@@ -0,0 +1,37 @@
+// 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+  }
+}
+
+function levelOrder(root: TreeNode | null): number[][] {
+  const result: number[][] = [];
+  if (!root) return result;
+  let layer = [root] as TreeNode[];
+  while (layer.length > 0) {
+    result.push(layer.map((node) => node.val));
+    layer = layer.map((node) => [node.left, node.right]).flat().filter((node) => node) as TreeNode[];
+  }
+  return result;
+}
