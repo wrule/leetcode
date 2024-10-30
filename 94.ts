aa72a849816e7fef644075904c1d44ca271ce173
jimao <shihao.gu@perfma.com>
2024-10-30 12:34:06 +0800

    94

--- 94.ts
+++ 94.ts
@@ -14,8 +14,37 @@
  * }
  */
 
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+  }
+}
+
 function inorderTraversal(root: TreeNode | null): number[] {
   const result: number[] = [];
-
+  const stack: [TreeNode, boolean][] = [];
+  let current = root;
+  while (current) {
+    if (current.right) stack.push([current.right, true]);
+    stack.push([current, false]);
+    if (current.left) stack.push([current.left, true]);
+    current = null;
+    while (stack.length > 0 && !current) {
+      const popData = stack.pop();
+      if (popData) {
+        const [next, flag] = popData;
+        if (flag) {
+          current = next;
+        } else {
+          result.push(next.val);
+        }
+      }
+    }
+  }
   return result;
 }
