b4f90a8a02553a7740396930e5c5529b3277d669
jimao <shihao.gu@perfma.com>
2024-10-30 11:40:14 +0800

    inorderTraversal

---
+++ 94.ts
@@ -0,0 +1,21 @@
+// 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  const result: number[] = [];
+
+  return result;
+}
