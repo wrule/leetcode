217c7968063e831ed323c3fc8acc2535d28018eb
jimao <shihao.gu@perfma.com>
2024-11-01 17:58:05 +0800

    133

---
+++ 133.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for _Node.
+ * class _Node {
+ *     val: number
+ *     neighbors: _Node[]
+ * 
+ *     constructor(val?: number, neighbors?: _Node[]) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.neighbors = (neighbors===undefined ? [] : neighbors)
+ *     }
+ * }
+ * 
+ */
+
+class _Node {
+  val: number
+  neighbors: _Node[]
+
+  constructor(val?: number, neighbors?: _Node[]) {
+    this.val = (val===undefined ? 0 : val)
+    this.neighbors = (neighbors===undefined ? [] : neighbors)
+  }
+}
+
+function cloneGraph(node: _Node | null): _Node | null {
+  return null;
+}
