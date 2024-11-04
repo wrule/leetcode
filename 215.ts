08b554082e8e4e426b55ba9434defa15bac5120c
jimao <shihao.gu@perfma.com>
2024-11-04 10:54:49 +0800

    提交

--- 215.ts
+++ 215.ts
@@ -12,6 +12,28 @@ function findKthLargest(nums: number[], k: number): number {
 
 function fastSort(nums: number[]) {
   let result = nums.slice();
+  function quickSort(arr: number[], left: number, right: number) {
+    if(left >= right) return;
+    
+    const pivot = arr[left];
+    let i = left;
+    let j = right;
+    
+    while(i < j) {
+      while(i < j && arr[j] >= pivot) j--;
+      while(i < j && arr[i] <= pivot) i++;
+      if(i < j) [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    
+    arr[left] = arr[i];
+    arr[i] = pivot;
+    
+    quickSort(arr, left, i - 1);
+    quickSort(arr, i + 1, right);
+  }
+  quickSort(result, 0, result.length - 1);
   // 这里实现快速排序
   return result;
 }
+
+console.log(fastSort([3, 1, 9, 2, 5, 5, 8]));
