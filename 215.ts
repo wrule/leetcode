7c69081e79e030eba4d1f32273631edff7ce6381
jimao <shihao.gu@perfma.com>
2024-11-04 12:01:02 +0800

    估计会超时

--- 215.ts
+++ 215.ts
@@ -2,36 +2,28 @@
 // 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
 // 你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
 
-function findKthLargest(nums: number[], k: number): number {
-  let result = 0;
-  nums.forEach((num) => {
-
-  });
-  return result;
-}
-
-function fastSort(nums: number[]) {
-  let result = nums.slice();
-  function quickSort(arr: number[], left: number, right: number) {
-    if (left >= right) return;
-    const randomIndex = left + Math.floor(Math.random() * (right - left + 1));
-    [arr[left], arr[randomIndex]] = [arr[randomIndex], arr[left]];
-    let mvLeft = left, mvRight = right;
-    const baseValue = arr[left];
-    while (mvLeft < mvRight) {
-      while (mvLeft < mvRight && arr[mvRight] >= baseValue) mvRight--;
-      while (mvLeft < mvRight && arr[mvLeft] <= baseValue) mvLeft++;
-      if (mvLeft < mvRight) {
-        [arr[mvLeft], arr[mvRight]] = [arr[mvRight], arr[mvLeft]];
-      }
+function quickSort(arr: number[], left: number, right: number) {
+  if (left >= right) return;
+  const randomIndex = left + Math.floor(Math.random() * (right - left + 1));
+  [arr[left], arr[randomIndex]] = [arr[randomIndex], arr[left]];
+  let mvLeft = left, mvRight = right;
+  const baseValue = arr[left];
+  while (mvLeft < mvRight) {
+    while (mvLeft < mvRight && arr[mvRight] >= baseValue) mvRight--;
+    while (mvLeft < mvRight && arr[mvLeft] <= baseValue) mvLeft++;
+    if (mvLeft < mvRight) {
+      [arr[mvLeft], arr[mvRight]] = [arr[mvRight], arr[mvLeft]];
     }
-    arr[left] = arr[mvLeft];
-    arr[mvLeft] = baseValue;
-    quickSort(arr, left, mvLeft - 1);
-    quickSort(arr, mvLeft + 1, right);
   }
-  quickSort(result, 0, result.length - 1);
-  return result;
+  arr[left] = arr[mvLeft];
+  arr[mvLeft] = baseValue;
+  quickSort(arr, left, mvLeft - 1);
+  quickSort(arr, mvLeft + 1, right);
 }
 
-console.log(fastSort([3, 1, 9, 2, 5, 5, 8]));
+function findKthLargest(nums: number[], k: number): number {
+  quickSort(nums, 0, nums.length - 1);
+  return nums[nums.length - k];
+}
+
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
