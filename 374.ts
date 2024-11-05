dc8dac1d7fef098cd8c4d3f5dd9709ea4b72566c
jimao <shihao.gu@perfma.com>
2024-11-05 14:22:43 +0800

    调整代码

--- 374.ts
+++ 374.ts
@@ -2,7 +2,7 @@
 class NumberMinHeap {
   public constructor(private readonly size: number) { }
 
-  public heap: number[] = [];
+  private heap: number[] = [];
 
   public Add(num: number) {
     if (this.heap.length < this.size) {
@@ -46,28 +46,6 @@ class NumberMinHeap {
         break;
       }
     }
-  }
-
-  public display() {
-    if (this.heap.length === 0) return "Empty heap";
-
-    const height = Math.floor(Math.log2(this.heap.length)) + 1;
-    const result: string[] = [];
-    
-    for (let level = 0; level < height; level++) {
-      const startIndex = Math.pow(2, level) - 1;
-      const endIndex = Math.min(Math.pow(2, level + 1) - 1, this.heap.length);
-      const levelNodes = this.heap.slice(startIndex, endIndex);
-      
-      // 计算当前层的缩进
-      const padding = " ".repeat(Math.pow(2, height - level - 1) - 1);
-      const spacing = " ".repeat(Math.pow(2, height - level) - 1);
-      
-      // 构建当前层的字符串
-      result.push(padding + levelNodes.join(spacing));
-    }
-
-    console.log(result.join("\n"));
   }
 }
 
