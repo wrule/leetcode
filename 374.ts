df5b0f1ac8edbb7855782bbda391034b7e1db53b
jimao <shihao.gu@perfma.com>
2024-11-05 13:28:00 +0800

    数组列表

--- 374.ts
+++ 374.ts
@@ -1,5 +1,9 @@
 
 function topKFrequent(nums: number[], k: number): number[] {
+  const hashMap: { [name: number]: number } = { };
+  nums.forEach((num) => hashMap[num] = (hashMap[num] ?? 0) + 1);
+  const mapList = Object.entries(hashMap).map(([name, value]) => ([Number(name), value]));
+  console.log(mapList);
   return [];
 }
 
