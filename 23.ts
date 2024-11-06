ed09307bd464a75c2cd68dcd096c563bab2d208d
jimao <shihao.gu@perfma.com>
2024-11-06 12:16:49 +0800

    把之前写过的小顶堆改一下

--- 23.ts
+++ 23.ts
@@ -13,6 +13,58 @@
  * }
  */
 
+class NumberMinHeap {
+  public constructor(private readonly size: number) { }
+
+  private heap: number[] = [];
+
+  public Add(num: number) {
+    if (this.heap.length < this.size) {
+      this.heap.push(num);
+      this.siftUp();
+    } else {
+      if (num > this.heap[0]) {
+        this.heap[0] = num;
+        this.siftDown();
+      }
+    }
+  }
+
+  private siftUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.heap[index] < this.heap[parentIndex]) {
+        [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  private siftDown() {
+    let index = 0;
+    while (true) {
+      let smallest = index;
+      const left = 2 * index + 1;
+      const right = 2 * index + 2;
+      if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
+        smallest = left;
+      }
+      if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
+        smallest = right;
+      }
+      if (smallest !== index) {
+        [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+        index = smallest;
+      } else {
+        break;
+      }
+    }
+  }
+}
+
 class ListNode {
   val: number
   next: ListNode | null
@@ -23,5 +75,6 @@ class ListNode {
 }
 
 function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
-  return null;
+  let result = new ListNode();
+  return result;
 }
