49e22665abab34347568f82b87d5d37b2e4b4c80
jimao <shihao.gu@perfma.com>
2024-11-06 15:29:24 +0800

    23

--- 23.ts
+++ 23.ts
@@ -13,95 +13,11 @@
  * }
  */
 
-class ListNode {
+class LinkNode {
   val: number
   next: ListNode | null
   constructor(val?: number, next?: ListNode | null) {
     this.val = (val===undefined ? 0 : val)
     this.next = (next===undefined ? null : next)
   }
-}
-
-class MinHeap<T extends { num: number }> {
-  public constructor(private readonly size: number) { }
-
-  private heap: T[] = [];
-
-  public Push(item: T) {
-    if (this.heap.length < this.size) {
-      this.heap.push(item);
-      this.siftUp();
-    }
-  }
-
-  public Shift(): T | null {
-    if (this.heap.length === 0) return null;
-    return this.heap.splice(0, 1)[0];
-  }
-
-  public ResetTop(item: T | null) {
-    if (item) {
-      this.heap.unshift(item);
-      this.siftDown();
-    } else if (this.heap.length >= 2) {
-      const last = this.heap.pop() as T;
-      this.heap.unshift(last);
-      this.siftDown();
-    }
-  }
-
-  private siftUp() {
-    let index = this.heap.length - 1;
-    while (index > 0) {
-      const parentIndex = Math.floor((index - 1) / 2);
-      if (this.heap[index].num < this.heap[parentIndex].num) {
-        [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
-        index = parentIndex;
-      } else {
-        break;
-      }
-    }
-  }
-
-  private siftDown() {
-    let index = 0;
-    while (true) {
-      let smallest = index;
-      const left = 2 * index + 1;
-      const right = 2 * index + 2;
-      if (left < this.heap.length && this.heap[left].num < this.heap[smallest].num) {
-        smallest = left;
-      }
-      if (right < this.heap.length && this.heap[right].num < this.heap[smallest].num) {
-        smallest = right;
-      }
-      if (smallest !== index) {
-        [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
-        index = smallest;
-      } else {
-        break;
-      }
-    }
-  }
-}
-
-function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
-  let result = new ListNode();
-  let current = result;
-  lists = lists.filter((list) => list);
-  const heap = new MinHeap<{ index: number, num: number }>(lists.length);
-  lists.forEach((first, index) => heap.Push({ index, num: first!.val }));
-  while (true) {
-    const newMinItem = heap.Shift();
-    if (!newMinItem) break;
-    current.next = new ListNode(newMinItem.num);
-    current = current.next;
-    lists[newMinItem.index] = lists[newMinItem.index]?.next ?? null;
-    heap.ResetTop(
-      lists[newMinItem.index] ?
-        { index: newMinItem.index, num: lists[newMinItem.index]!.val } :
-        null
-    );
-  }
-  return result.next;
 }
