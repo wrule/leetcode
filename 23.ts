8fdb74ee8ef2140fd998a4ea37ecc94a09c4cedc
jimao <shihao.gu@perfma.com>
2024-11-06 11:32:54 +0800

    23

---
+++ 23.ts
@@ -0,0 +1,18 @@
+// 给你一个链表数组，每个链表都已经按升序排列。
+// 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+
+}
