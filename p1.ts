4a3eafadf040b75422322e079e36fd0cd6f229c3
jimao <shihao.gu@perfma.com>
2024-11-07 18:23:40 +0800

    恢复注释

--- p1.ts
+++ p1.ts
@@ -1,93 +1,93 @@
 
-// enum MyPromiseState {
-//   PENDING = 'PENDING',
-//   FULFILLED = 'FULFILLED',
-//   REJECTED = 'REJECTED',
-// }
-
-// class MyPromise<T> {
-//   public constructor(
-//     private readonly executor: (
-//       resolve: (value: T | PromiseLike<T>) => void,
-//       reject: (reason?: any) => void,
-//     ) => void,
-//   ) {
-//     const resolve = (value: T | PromiseLike<T>) => {
-//       if (this.state === MyPromiseState.PENDING) {
-//         this.state = MyPromiseState.FULFILLED;
-//         this.value = value;
-//       }
-//     };
-//     const reject = (reason?: any) => {
-//       if (this.state === MyPromiseState.PENDING) {
-//         this.state = MyPromiseState.REJECTED;
-//       }
-//     };
-//     try {
-//       this.executor(resolve, reject);
-//     } catch (error) {
-//       this.state = MyPromiseState.REJECTED;
-//       reject(error);
-//     }
-//   }
-
-//   private state = MyPromiseState.PENDING;
-//   private value!: T | PromiseLike<T>;
-//   private error: any = null;
-
-//   private resolve(value: T | PromiseLike<T>) {
-//     if (this.state === MyPromiseState.PENDING) {
-//       this.state = MyPromiseState.FULFILLED;
-//       this.value = value;
-//     }
-//   }
-
-//   private reject(reason?: any) {
-//     if (this.state === MyPromiseState.PENDING) {
-//       this.state = MyPromiseState.REJECTED;
-//       this.error = reason;
-//     }
-//   }
-
-//   public then(
-//     onfulfilled?: ((value: T) => T | PromiseLike<T>) | null,
-//     onrejected?: ((reason: any) => PromiseLike<never>) | null,
-//   ): Promise<T> {
-//     throw '';
-//   }
-
-//   public catch(
-//     onrejected?: ((reason: any) => PromiseLike<never>) | null,
-//   ): Promise<T> {
-//     throw '';
-//   }
-
-//   public finally(
-//     onfinally?: (() => void) | null,
-//   ): Promise<T> {
-//     throw '';
-//   }
-// }
-
-const a = new Promise<number>((resolve, reject) => {
-  setTimeout(() => {
-    resolve(1234);
-  }, 1000);
-});
-
-async function main() {
-  a.then((result) => {
-    console.log('then1', result);
-    throw '新错误1';
-  }, (reason) => {
-    console.log('then2', reason);
-    throw '新错误2';
-  }).catch((reason) => {
-    console.log('catch', reason);
-    throw '新错误3';
-  }).finally(() => {
-    console.log('finally');
-  });
+enum MyPromiseState {
+  PENDING = 'PENDING',
+  FULFILLED = 'FULFILLED',
+  REJECTED = 'REJECTED',
 }
 
-main();
+class MyPromise<T> {
+  public constructor(
+    private readonly executor: (
+      resolve: (value: T | PromiseLike<T>) => void,
+      reject: (reason?: any) => void,
+    ) => void,
+  ) {
+    const resolve = (value: T | PromiseLike<T>) => {
+      if (this.state === MyPromiseState.PENDING) {
+        this.state = MyPromiseState.FULFILLED;
+        this.value = value;
+      }
+    };
+    const reject = (reason?: any) => {
+      if (this.state === MyPromiseState.PENDING) {
+        this.state = MyPromiseState.REJECTED;
+      }
+    };
+    try {
+      this.executor(resolve, reject);
+    } catch (error) {
+      this.state = MyPromiseState.REJECTED;
+      reject(error);
+    }
+  }
+
+  private state = MyPromiseState.PENDING;
+  private value!: T | PromiseLike<T>;
+  private error: any = null;
+
+  private resolve(value: T | PromiseLike<T>) {
+    if (this.state === MyPromiseState.PENDING) {
+      this.state = MyPromiseState.FULFILLED;
+      this.value = value;
+    }
+  }
+
+  private reject(reason?: any) {
+    if (this.state === MyPromiseState.PENDING) {
+      this.state = MyPromiseState.REJECTED;
+      this.error = reason;
+    }
+  }
+
+  public then(
+    onfulfilled?: ((value: T) => T | PromiseLike<T>) | null,
+    onrejected?: ((reason: any) => PromiseLike<never>) | null,
+  ): Promise<T> {
+    throw '';
+  }
+
+  public catch(
+    onrejected?: ((reason: any) => PromiseLike<never>) | null,
+  ): Promise<T> {
+    throw '';
+  }
+
+  public finally(
+    onfinally?: (() => void) | null,
+  ): Promise<T> {
+    throw '';
+  }
+}
+
+// const a = new Promise<number>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(1234);
+//   }, 1000);
+// });
+
+// async function main() {
+//   a.then((result) => {
+//     console.log('then1', result);
+//     throw '新错误1';
+//   }, (reason) => {
+//     console.log('then2', reason);
+//     throw '新错误2';
+//   }).catch((reason) => {
+//     console.log('catch', reason);
+//     throw '新错误3';
+//   }).finally(() => {
+//     console.log('finally');
+//   });
+// }
+
+// main();
