c145efc4da6f3687b0f3247aee984f6c8f492ac6
jimao <shihao.gu@perfma.com>
2024-11-07 19:25:35 +0800

    提交

--- p1.ts
+++ p1.ts
@@ -4,7 +4,7 @@ function test() {
   const a = new Promise<number>((resolve, reject) => {
     setTimeout(() => {
       resolve(1234);
-    }, 1000);
+    }, 500);
   });
   a.then(console.log);
   a.then(console.log);
