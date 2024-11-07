d165dd716694dfc08a4a6950824cca67f21a9260
jimao <shihao.gu@perfma.com>
2024-11-07 19:27:36 +0800

    复制

--- p1.ts
+++ p1.ts
@@ -1,6 +1,15 @@
 
 
+class MyPromise<T> {
+  public constructor(
+    private readonly executor: (resolve: (value: number | PromiseLike<number>) => void, reject: (reason?: any) => void) => void,
+  ) {
+
+  }
+}
+
 function test() {
+  // const b = new Promise<number>
   const a = new Promise<number>((resolve, reject) => {
     setTimeout(() => {
       resolve(1234);
