08a672f70439939d9bc55339967a537bda64c414
jimao <shihao.gu@perfma.com>
2024-11-11 10:00:25 +0800

    选择排序

---
+++ x5.ts
@@ -0,0 +1,18 @@
+// 使用JavaScript实现选择排序
+// // 测试数据
+// let arr = [5, 3, 2, 4, 1];
+// console.log(selectionSort(arr)); // 输出: [1, 2, 3, 4, 5]
+
+function selectionSort(numList: number[]) {
+  for (let i = 0; i < numList.length - 1; ++i) {
+    let minIndex = i;
+    for (let j = i + 1; j < numList.length; ++j) {
+      if (numList[j] < numList[i]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      [numList[minIndex], numList[i]] = [numList[i], numList[minIndex]];
+    }
+  }
+}
