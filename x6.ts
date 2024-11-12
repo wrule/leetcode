677e213fb32425368289b73d8ff9f3b586a35571
jimao <shihao.gu@perfma.com>
2024-11-12 12:28:53 +0800

    insertionSort

--- x6.ts
+++ x6.ts
@@ -2,8 +2,19 @@
 // let array = [12, 11, 13, 5, 6];
 // console.log("Sorted array is:", insertionSort(array));
 
-function insertionSort(arr: number[]) {
-
+function insertionSort(numList: number[]) {
+  for (let i = 1; i < numList.length; ++i) {
+    const current = numList[i];
+    for (let j = i - 1; j >= 0; --j) {
+      if (numList[j] > current) {
+        numList[j + 1] = numList[j];
+      } else {
+        numList[j + 1] = current;
+        break;
+      }
+    }
+  }
+  return numList;
 }
 
 console.log(insertionSort([12, 11, 13, 5, 6]));
