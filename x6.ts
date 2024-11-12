40b02ea36e7af0a8ad44d8f067c60ae247505bcf
jimao <shihao.gu@perfma.com>
2024-11-12 12:22:20 +0800

    删除掉

--- x6.ts
+++ x6.ts
@@ -3,16 +3,7 @@
 // console.log("Sorted array is:", insertionSort(array));
 
 function insertionSort(arr: number[]) {
-  for (let i = 1; i < arr.length; i++) {
-    const current = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > current) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = current;
-  }
-  return arr;
+
 }
 
 console.log(insertionSort([12, 11, 13, 5, 6]));
