cf98ce7e909e59adedbf3d9b4e986cc633ab8a33
jimao <shihao.gu@perfma.com>
2024-11-13 10:34:36 +0800

    提交

--- x7.ts
+++ x7.ts
@@ -1,2 +1,15 @@
+// 使用JavaScript实现归并排序
+// let array = [4, 2, 5, 1, 6, 3];
+// console.log("Sorted array is:", mergeSort(array));
 
-console.log('你好，世界');
+function mergeSort(numList: number[]): number[] {
+  console.log(numList);
+  console.log();
+  if (numList.length <= 1) return numList.slice();
+  const rightStartIndex = Math.floor(numList.length / 2);
+  const leftList = mergeSort(numList.slice(0, rightStartIndex));
+  const rightList = mergeSort(numList.slice(rightStartIndex));
+  return leftList.concat(rightList);
+}
+
+console.log(mergeSort([4, 2, 5, 1, 6, 3]));
