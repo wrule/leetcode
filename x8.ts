5d0c4d18f2523c1e66de2925d4f24e71441a9dd6
jimao <shihao.gu@perfma.com>
2024-11-14 12:15:06 +0800

    console.log

--- x8.ts
+++ x8.ts
@@ -4,20 +4,20 @@
 // console.log(quickSort(arr));  // 输出: [2, 3, 5, 6, 7, 9]
 
 function quickSort(numList: number[]) {
-  console.log('init', numList);
+  // console.log('init', numList);
   const randomIndex = Math.floor(Math.random() * numList.length);
   const randomNum = numList[randomIndex];
   [numList[0], numList[randomIndex]] = [numList[randomIndex], numList[0]];
-  console.log(randomIndex, randomNum);
-  console.log('swap', numList);
+  // console.log(randomIndex, randomNum);
+  // console.log('swap', numList);
   let leftIndex = 1, rightIndex = numList.length - 1;
   while (leftIndex < rightIndex) {
     while (leftIndex < rightIndex && numList[rightIndex] >= randomNum) rightIndex--;
     while (leftIndex < rightIndex && numList[leftIndex] <= randomNum) leftIndex++;
     if (leftIndex < rightIndex) {
-      console.log('snap', numList[leftIndex], numList[rightIndex]);
+      // console.log('snap', numList[leftIndex], numList[rightIndex]);
       [numList[leftIndex], numList[rightIndex]] = [numList[rightIndex], numList[leftIndex]];
-      console.log(numList);
+      // console.log(numList);
     }
   }
   numList[0] = numList[leftIndex];
