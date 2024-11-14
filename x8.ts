35cdc28934137116882d5b70f36379481a43b70c
jimao <shihao.gu@perfma.com>
2024-11-14 12:30:03 +0800

    x8有待完善

--- x8.ts
+++ x8.ts
@@ -3,27 +3,29 @@
 // var arr = [5, 3, 7, 6, 2, 9];
 // console.log(quickSort(arr));  // 输出: [2, 3, 5, 6, 7, 9]
 
-function quickSort(numList: number[]) {
-  // console.log('init', numList);
+function quickSort(numList: number[]): number[] {
+  if (numList.length <= 1) return numList;
   const randomIndex = Math.floor(Math.random() * numList.length);
-  const randomNum = numList[randomIndex];
+  const pivot = numList[randomIndex];
   [numList[0], numList[randomIndex]] = [numList[randomIndex], numList[0]];
-  // console.log(randomIndex, randomNum);
-  // console.log('swap', numList);
   let leftIndex = 1, rightIndex = numList.length - 1;
-  while (leftIndex < rightIndex) {
-    while (leftIndex < rightIndex && numList[rightIndex] >= randomNum) rightIndex--;
-    while (leftIndex < rightIndex && numList[leftIndex] <= randomNum) leftIndex++;
-    if (leftIndex < rightIndex) {
-      // console.log('snap', numList[leftIndex], numList[rightIndex]);
+  while (leftIndex <= rightIndex) {
+    while (leftIndex <= rightIndex && numList[rightIndex] >= pivot) rightIndex--;
+    while (leftIndex <= rightIndex && numList[leftIndex] <= pivot) leftIndex++;
+    if (leftIndex <= rightIndex) {
       [numList[leftIndex], numList[rightIndex]] = [numList[rightIndex], numList[leftIndex]];
-      // console.log(numList);
     }
   }
-  numList[0] = numList[leftIndex];
-  numList[leftIndex] = randomNum;
-  return numList;
+  if (numList[rightIndex] < pivot) {
+    [numList[0], numList[rightIndex]] = [numList[rightIndex], pivot];
+  }
+  return [
+    ...quickSort(numList.slice(0, rightIndex)),
+    numList[rightIndex],
+    ...quickSort(numList.slice(rightIndex + 1)),
+  ];
 }
 
 console.log(quickSort([5, 3, 7, 6, 2, 9, 11, 0, 8, 4]));
 // console.log(quickSort([5, 3, 7, 6, 2, 9, 11, 0, 8, 4]));
+
