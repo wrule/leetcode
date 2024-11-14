907c4e889b51d99c0b34d91d0d16114ff5e5298f
jimao <shihao.gu@perfma.com>
2024-11-14 12:09:35 +0800

    提交代码

--- x8.ts
+++ x8.ts
@@ -4,22 +4,26 @@
 // console.log(quickSort(arr));  // 输出: [2, 3, 5, 6, 7, 9]
 
 function quickSort(numList: number[]) {
-  const randomIndex = Math.floor(Math.random() * numList.length);
+  console.log('init', numList);
+  const randomIndex = 8;
   const randomNum = numList[randomIndex];
+  [numList[0], numList[randomIndex]] = [numList[randomIndex], numList[0]];
   console.log(randomIndex, randomNum);
+  console.log('swap', numList);
   let leftIndex = 0, rightIndex = numList.length - 1;
   while (leftIndex < rightIndex) {
     while (leftIndex < rightIndex && numList[rightIndex] >= randomNum) rightIndex--;
     while (leftIndex < rightIndex && numList[leftIndex] <= randomNum) leftIndex++;
     if (leftIndex < rightIndex) {
+      console.log('snap', numList[leftIndex], numList[rightIndex]);
       [numList[leftIndex], numList[rightIndex]] = [numList[rightIndex], numList[leftIndex]];
+      console.log(numList);
     }
   }
-  numList[randomIndex] = numList[leftIndex];
+  numList[0] = numList[leftIndex];
   numList[leftIndex] = randomNum;
   return numList;
 }
 
 console.log(quickSort([5, 3, 7, 6, 2, 9, 11, 0, 8, 4]));
-console.log([5, 3, 7, 6, 2, 9, 11, 0, 8, 4]);
 // console.log(quickSort([5, 3, 7, 6, 2, 9, 11, 0, 8, 4]));
