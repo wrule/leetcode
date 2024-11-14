d02b37e7760de3740664a233285ef6b4f0d9d37f
jimao <shihao.gu@perfma.com>
2024-11-14 11:08:02 +0800

    x8

---
+++ x8.ts
@@ -0,0 +1,10 @@
+// 使用JavaScript实现快速排序
+// // 测试
+// var arr = [5, 3, 7, 6, 2, 9];
+// console.log(quickSort(arr));  // 输出: [2, 3, 5, 6, 7, 9]
+
+function quickSort(numList: number[]) {
+  return numList;
+}
+
+console.log([5, 3, 7, 6, 2, 9]);
