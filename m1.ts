3b899344b11fe6a4367d049b14e7e876e1a92252
jimao <shihao.gu@perfma.com>
2024-11-19 00:35:19 +0800

    m1

--- m1.ts
+++ m1.ts
@@ -15,6 +15,14 @@ function sum(a: number, b: number, c: number) {
   return a + b + c;
 }
 
+function sayHello(text: string, count: number) {
+  for (let i = 0; i < count; ++i) {
+    console.log('hello', text, i);
+  }
+}
+
 console.log(sum(1, 2, 3));
 const currySum = curry(sum);
 console.log(currySum(1)(2)(3));
+const curryHello = curry(sayHello);
+curryHello('少打打分')(2);
