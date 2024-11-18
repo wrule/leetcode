7611f501147e5398b36e6f08e78fb68c00742550
jimao <shihao.gu@perfma.com>
2024-11-19 00:27:35 +0800

    科利华完成

--- m1.ts
+++ m1.ts
@@ -1,10 +1,21 @@
 
 function curry(fn: Function) {
-  console.log(fn.length);
+  const nextCurry = (...args: number[]) => {
+    if (args.length >= fn.length) {
+      return fn(...args);
+    }
+    return (...nextArgs: number[]) => {
+      return nextCurry(...args, ...nextArgs);
+    };
+  };
+  return nextCurry;
 }
+
 
 function sum(a: number, b: number, c: number) {
   return a + b + c;
 }
 
-curry(sum);
+console.log(sum(1, 2, 3));
+const currySum = curry(sum);
+console.log(currySum(1)(2)(3));
