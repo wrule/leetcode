178050b148a7eb331e2c3ddec11f5ce3f4e94a86
jimao <shihao.gu@perfma.com>
2024-11-19 10:11:31 +0800

    21

---
+++ 21.ts
@@ -0,0 +1,17 @@
+// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+  return null;
+}
