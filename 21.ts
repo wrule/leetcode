6e012a40703359c5a7cec168b2d7013c5c376598
jimao <shihao.gu@perfma.com>
2024-11-19 10:59:11 +0800

    21

--- 21.ts
+++ 21.ts
@@ -12,6 +12,36 @@
  * }
  */
 
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+  }
+}
+
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-  return null;
+  const root = new ListNode();
+  let current = root;
+  let list1Node = list1, list2Node = list2;
+  while (list1Node || list2Node) {
+    if (list1Node && list2Node) {
+      if (list1Node.val <= list2Node.val) {
+        current.next = new ListNode(list1Node.val);
+        list1Node = list1Node.next;
+      } else {
+        current.next = new ListNode(list2Node.val);
+        list2Node = list2Node.next;
+      }
+    } else if (list1Node) {
+      current.next = new ListNode(list1Node.val);
+      list1Node = list1Node.next;
+    } else {
+      current.next = new ListNode(list2Node!.val);
+      list2Node = list2Node!.next;
+    }
+    current = current.next!;
+  }
+  return root.next;
 }
