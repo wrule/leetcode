35b253bc1bb82e35b892f4b4a1ab88b5e3fb4429
jimao <shihao.gu@perfma.com>
2024-11-21 12:55:52 +0800

    226

--- 226.ts
+++ 226.ts
@@ -13,7 +13,25 @@
  * }
  */
 
-function invertTree(root: TreeNode | null): TreeNode | null {
-  return null;
+class TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+  }
 }
 
+function invertTree(root: TreeNode | null): TreeNode | null {
+  if (!root) return null;
+  const stack = [root];
+  while (stack.length >= 1) {
+    const current = stack.pop()!;
+    [current.right, current.left] = [current.left, current.right];
+    if (current.left) stack.push(current.left);
+    if (current.right) stack.push(current.right);
+  }
+  return root;
+}
