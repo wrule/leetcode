8c8285dda3a4c93aa1ae4fe227e99e9ca0d3fab9
jimao <shihao.gu@perfma.com>
2024-11-21 12:02:48 +0800

    226

---
+++ 226.ts
@@ -0,0 +1,19 @@
+// 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  return null;
+}
+
