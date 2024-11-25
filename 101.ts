dd1db131d3d33a4ced7509b6b1870a7b279fedaa
jimao <shihao.gu@perfma.com>
2024-11-25 10:49:54 +0800

    101

--- 101.ts
+++ 101.ts
@@ -25,11 +25,42 @@ class TreeNode {
   }
 }
 
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (p && q) {
+    const stack1 = [p];
+    const stack2 = [q];
+    while (stack1.length >= 1 && stack2.length >= 1) {
+      const current1 = stack1.pop()!;
+      const current2 = stack2.pop()!;
+      if (current1.val === current2.val) {
+        if (
+          !!current1.left !== !!current2.left ||
+          !!current1.right !== !!current2.right
+        ) {
+          return false;
+        }
+        if (current1.right) stack1.push(current1.right);
+        if (current1.left) stack1.push(current1.left);
+        if (current2.right) stack2.push(current2.right);
+        if (current2.left) stack2.push(current2.left);
+      } else {
+        return false;
+      }
+    }
+    return stack1.length === stack2.length;
+  } else if (!p && !q) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function isSymmetric(root: TreeNode | null): boolean {
   if (!root) return true;
-  const queue = [root.left, root.right].filter((node) => node);
-  while (queue.length >= 1) {
-    
+  if (!root.left && !root.right) return true;
+  else if (root.left && root.right) {
+    return isSameTree(root.left, root.right);
+  } else {
+    return false;
   }
-  return true;
 }
