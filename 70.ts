0fff73a78098796e1547b6c187b373a520ad8fb2
jimao <shihao.gu@perfma.com>
2024-11-26 10:41:00 +0800

    70

--- 70.ts
+++ 70.ts
@@ -4,7 +4,7 @@
 function climbStairs(n: number): number {
   if (n === 1) return 1;
   else if (n === 2) return 2;
-  return climbStairs(n - 1) + climbStairs(n - 2);
+  return climbStairs(n - 1) + climbStairs(n - 2) + 1;
 }
 
-console.log(climbStairs(1));
+console.log(climbStairs(3));
