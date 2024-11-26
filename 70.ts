1c62eef5c58cf194111e1d6ed9b8f50213e4d2ec
jimao <shihao.gu@perfma.com>
2024-11-26 10:38:21 +0800

    提交

--- 70.ts
+++ 70.ts
@@ -2,5 +2,9 @@
 // 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
 
 function climbStairs(n: number): number {
-  return 0;
+  if (n === 1) return 1;
+  else if (n === 2) return 2;
+  return climbStairs(n - 1) + climbStairs(n - 2);
 }
+
+console.log(climbStairs(1));
