f9935d9b4b4e3677c45fd5384509768b44cf0abf
jimao <shihao.gu@perfma.com>
2024-11-27 10:24:21 +0800

    rob

--- 198.ts
+++ 198.ts
@@ -2,7 +2,10 @@
 // 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
 
 function rob(nums: number[]): number {
+  if (nums.length === 0) return 0;
+  else if (nums.length === 1) return nums[0];
+  else if (nums.length === 2) 
   return 0;
 }
 
-
+console.log(rob([2, 7, 9, 3, 1]));
