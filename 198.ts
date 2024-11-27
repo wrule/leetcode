477c29a49e1798739e06de73e150e9bfab417033
jimao <shihao.gu@perfma.com>
2024-11-27 10:29:46 +0800

    198

--- 198.ts
+++ 198.ts
@@ -5,6 +5,7 @@ function rob(nums: number[]): number {
   if (nums.length === 0) return 0;
   else if (nums.length === 1) return nums[0];
   else if (nums.length === 2) return Math.max(nums[0], nums[1]);
+  else if (nums.length === 3) 
   return 0;
 }
 
