0d1b9860e71bfbf645b683c3a602ce96ca454ddc
jimao <shihao.gu@perfma.com>
2024-11-28 10:35:46 +0800

    455

--- 455.ts
+++ 455.ts
@@ -2,5 +2,7 @@
 // 对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j] 。如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是满足尽可能多的孩子，并输出这个最大数值。
 
 function findContentChildren(g: number[], s: number[]): number {
+  const maxS = Math.max(...s);
+  const newG = g.filter((num) => num <= maxS);
   return 0;
 }
