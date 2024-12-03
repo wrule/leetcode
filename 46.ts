2971633b7a641cbff6868cd887027f106cd730be
jimao <shihao.gu@perfma.com>
2024-12-03 11:09:34 +0800

    46

--- 46.ts
+++ 46.ts
@@ -1,5 +1,15 @@
 // 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
 
 function permute(nums: number[]): number[][] {
-  return [];
+  if (nums.length <= 1) return [nums];
+  const result: number[][] = [];
+  nums.forEach((num) => {
+    const others = nums.filter((item) => item !== num);
+    permute(others).forEach((subArray) => {
+      result.push([num].concat(subArray));
+    });
+  });
+  return result;
 }
+
+console.log(permute([1, 2, 3, 4]));
