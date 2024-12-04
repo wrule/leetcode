ab4a1bac637f1dfffde510407c2b9938c81a7146
jimao <shihao.gu@perfma.com>
2024-12-04 13:10:20 +0800

    78

--- 78.ts
+++ 78.ts
@@ -10,3 +10,24 @@ function subsets(nums: number[]): number[][] {
 }
 
 console.log(subsets([1, 2, 3]));
+
+// Ts每日一题
+
+
+interface TodoA {
+  readonly title: string;
+  readonly description: string;
+  completed: boolean;
+}
+
+type Equal<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2)
+    ? true
+    : false;
+
+type GetReadonlyKeys<T> = keyof {
+  [P in keyof T as Equal<{ [Q in P]: T[P] }, { readonly [Q in P]: T[P] }> extends true ? P : never]: T[P]
+};
+
+type Keys = GetReadonlyKeys<TodoA>;
