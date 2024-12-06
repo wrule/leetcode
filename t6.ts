ceb4657e05bb13b6c1e5bcbda84099c1a18f841b
jimao <shihao.gu@perfma.com>
2024-12-06 18:25:15 +0800

    提交

---
+++ t6.ts
@@ -0,0 +1,21 @@
+// Implement the built-in Readonly<T> generic without using it.
+// Constructs a type with all properties of T set to readonly, meaning the properties of the constructed type cannot be reassigned.
+// For example:
+
+interface Todo2 {
+  title: string;
+  description: string;
+}
+
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P];
+};
+
+const todo: MyReadonly<Todo2> = {
+  title: "Hey",
+  description: "foobar",
+}
+
+// todo.title = "Hello" // Error: cannot reassign a readonly property
+
+// todo.description = "barFoo" // Error: cannot reassign a readonly property
