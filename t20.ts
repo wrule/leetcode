d2e615b08638126981b49b7899f15463682ea2fd
jimao <shihao.gu@perfma.com>
2024-12-27 12:50:17 +0800

    feat: t20

--- t20.ts
+++ t20.ts
@@ -2,16 +2,18 @@
 // For example
 
 type WhitespaceString = ' ' | '\t' | '\n' | '\r';
+
 type TrimLeft<T extends string> =
   T extends `${WhitespaceString}${infer R}` ? TrimLeft<R> : T;
 
-type trimed = TrimLeft<'  Hello World  '> // expected to be 'Hello World  '
+type TrimRight<T extends string> =
+  T extends `${infer L}${WhitespaceString}` ? TrimRight<L> : T;
 
+type trimed = TrimLeft<'  Hello World  '> // expected to be 'Hello World  '
 
 // Implement Trim<T> which takes an exact string type and returns a new string with the whitespace from both ends removed.
 // For example
 
-type Trim<T extends string> =
-  T extends `${WhitespaceString}${infer M}${WhitespaceString}` ? Trim<M> : T;
+type Trim<T extends string> = TrimRight<TrimLeft<T>>;
 
 type trimmed = Trim<'  Hello World  '> // expected to be 'Hello World'
