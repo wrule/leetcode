094cd377db4b9b246feabcd4378e4e8e8693ec80
jimao <shihao.gu@perfma.com>
2024-12-31 11:34:25 +0800

    feat: t22

--- t22.ts
+++ t22.ts
@@ -3,10 +3,14 @@
 
 type Test1 = [1, 1, 1]
 type Test2 = [1, 1, 2]
+type Test3 = ['jimao', 'jimao', 'jimao']
 
 type MyAll<T extends any[], A> = T extends [infer B, ...infer C] ? (
-  B extends A ? MyAll<C, A> : false
-) : true;
+  C extends [] ? (B extends A ? true : false) : (
+    MyAll<C, A>
+  )
+) : never;
 
 type _Todo1 = MyAll<Test1, 1> // should be same as true
 type _Todo2 = MyAll<Test2, 1> // should be same as false
+type _Todo3 = MyAll<Test3, 'jimao'> // should be same as false
